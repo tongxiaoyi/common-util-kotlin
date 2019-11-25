8a35adb7d227d69e98ac9635fd0b912b38d380e2
oneliang <oneliang@tencent.com>
2019-11-25 23:52:13 +0800

    no need to + FileUtilExtensions.kt

--- src/main/kotlin/com/oneliang/ktx/util/file/FileUtilExtensions.kt
+++
@@ -1,8 +0,0 @@
-package com.oneliang.ktx.util.file
-
-import java.io.InputStream
-import java.io.OutputStream
-
-fun InputStream.copyTo(outputStream: OutputStream) {
-    FileUtil.copyStream(this, outputStream)
-}
