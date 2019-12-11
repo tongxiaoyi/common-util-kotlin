c525adaf020fbf643845abd971da51736397986e
oneliang <oneliang@tencent.com>
2019-12-11 23:00:29 +0800

    optimize ArrayExtensions.kt

--- src/main/kotlin/com/oneliang/ktx/util/common/ArrayExtensions.kt
+++ src/main/kotlin/com/oneliang/ktx/util/common/ArrayExtensions.kt
@@ -1,5 +1,9 @@
 package com.oneliang.ktx.util.common
 
+fun <T, K, V> Array<T>.toMap(transform: (T) -> Pair<K, V>): Map<K, V> = this.toMapWithIndex { _, t ->
+    transform(t)
+}
+
 fun <T, K, V> Array<out T>.toMapWithIndex(transform: (index: Int, t: T) -> Pair<K, V>): Map<K, V> {
     val map = mutableMapOf<K, V>()
     this.forEachIndexed { index, t ->
